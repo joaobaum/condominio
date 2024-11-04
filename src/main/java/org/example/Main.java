55639b51fbbfa5c6a0fb8de71096beb05fa5a1c5
Joao Baum <joaobaum1@gmail.com>
2024-11-04 16:08:17 -0300

    Regras do condominio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String condominio1 = "condominio";
+        String condominio3 = "ENSEADA";
+        String condominio2 = "dias trabalhados";
+        int condominio = 15;
+        int condominio4 = 260;
+        int condominio5 = 200;
+        String condominio06 = "valores por Serviços";
+        System.out.println(condominio3);
+        System.out.println(condominio1);
+        System.out.println(condominio2);
+        System.out.println(condominio);
+        System.out.println(condominio06);
+        System.out.println(condominio4);
+        System.out.println(condominio5);
     }
 }
