2f3b7eb13330205f89bb2508d55752956636b37b
Joao Baum <joaobaum1@gmail.com>
2024-11-11 11:12:40 -0300

    alteraçao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,36 @@
 package org.example;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        String condominio1 = "condominio";
-        String condominio3 = "ENSEADA";
-        String condominio2 = "dias trabalhados";
-        int condominio = 15;
-        int condominio4 = 260;
-        int condominio5 = 200;
-        String condominio06 = "valores por Serviços";
-        System.out.println(condominio3);
-        System.out.println(condominio1);
-        System.out.println(condominio2);
-        System.out.println(condominio);
-        System.out.println(condominio06);
-        System.out.println(condominio4);
-        System.out.println(condominio5);
+
+        LocalDate data1 = LocalDate.of(2024, 11, 1);
+        LocalDate data2 = LocalDate.of(2024, 11, 3);
+        LocalDate data3 = LocalDate.of(2024, 11, 5);
+        LocalDate data4 = LocalDate.of(2024, 11, 7);
+        LocalDate data5 = LocalDate.of(2024, 11, 9);
+
+
+        calcularValores(0, data1,data2, data3, data4,data5);
+
     }
+
+    private static void calcularValores(int quantosFeriados, LocalDate... datasDeTrabalho) {
+        double total = 0.0;
+        for (LocalDate data : datasDeTrabalho) {
+            total += validarValoresReceber(data);
+        }
+        System.out.printf("Valores total = R$%,.2f%n", total - quantosFeriados * 60);
+    }
+
+    private static double validarValoresReceber(LocalDate localDate) {
+        double valoresDiasUteis = 260.0;
+        double valoresFindFeriado = 200.0;
+
+        return (localDate.getDayOfWeek() == DayOfWeek.SATURDAY || localDate.getDayOfWeek() == DayOfWeek.SUNDAY) ? valoresFindFeriado : valoresDiasUteis;
+    }
+
+
 }
